bb6799cb0a4e4539942c999d1bff9ca77e8339ff
Daniel Garcia Mandillo <daniel.garciamandillo@gmail.com>
2016-10-06 16:31:56 +0200

    polymer compress tasks added

--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -1,15 +1,17 @@
-const env = require('dotenv').config({ silent: true });
-const webpack = require('webpack');
-const path = require('path');
-const _ = require('lodash');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+let env = require('dotenv').config({
+    silent: true
+});
+let webpack = require('webpack');
+let path = require('path');
+let _ = require('lodash');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let CopyWebpackPlugin = require('copy-webpack-plugin');
+let helpers = require('./helpers');
+let letants = require('./constants');
 
-const helpers = require('./helpers');
-const letants = require('./constants');
-
-const isProd = process.env.npm_lifecycle_event === 'build';
-const envMap = _.mapValues(env, v => JSON.stringify(v));
+let isProd = process.env.npm_lifecycle_event === 'build';
+let envMap = _.mapValues(env, v => JSON.stringify(v));
 
 module.exports = {
     entry: {
@@ -25,42 +27,39 @@ module.exports = {
 
     module: {
         rules: [{
-                enforce: 'pre',
-                test: /^((?!(ngfactory|shim)).)*ts$/,
-                loader: 'tslint'
-            },
-            {
-                test: /\.ts$/,
-                loaders: [
-                    'awesome-typescript-loader',
-                    'angular2-template-loader',
-                    'angular2-router-loader?loader=system&genDir=src/compiled/src/app&aot=' + isProd
-                ]
-            },
-            {
-                test: /\.html$/,
-                loader: 'html',
-                exclude: helpers.root('src', 'public')
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: 'file?name=assets/[name].[hash].[ext]'
-            },
-            {
-                test: /\.css$/,
-                exclude: helpers.root('src', 'app'),
-                loader: ExtractTextPlugin
-                    .extract({
-                        fallbackLoader: "style-loader",
-                        loader: "css-loader" + (isProd ? '?minimize' : '')
-                    })
-            },
-            {
-                test: /\.css$/,
-                include: helpers.root('src', 'app'),
-                loader: 'raw'
-            }
-        ]
+            enforce: 'pre',
+            test: /^((?!(ngfactory|shim)).)*ts$/,
+            loader: 'tslint'
+        }, {
+            test: /\.ts$/,
+            loaders: [
+                'awesome-typescript-loader',
+                'angular2-template-loader',
+                'angular2-router-loader?loader=system&genDir=src/compiled/src/app&aot=' + isProd
+            ]
+        }, {
+            test: /\.html$/,
+            loader: 'html',
+            exclude: helpers.root('src', 'public')
+        }, {
+            test: /polymer\.vulcan\.html$/,
+            loader: 'file?name=[name]-[hash:6].[ext]'
+        }, {
+            test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: 'file?name=assets/[name].[hash].[ext]'
+        }, {
+            test: /\.css$/,
+            exclude: helpers.root('src', 'app'),
+            loader: ExtractTextPlugin
+                .extract({
+                    fallbackLoader: "style-loader",
+                    loader: "css-loader" + (isProd ? '?minimize' : '')
+                })
+        }, {
+            test: /\.css$/,
+            include: helpers.root('src', 'app'),
+            loader: 'raw'
+        }]
     },
 
     plugins: [
@@ -69,7 +68,6 @@ module.exports = {
         }),
 
         new webpack.ContextReplacementPlugin(
-            // The (\\|\/) piece accounts for path separators in *nix and Windows
             letants.CONTEXT_REPLACE_REGEX,
             helpers.root('./src') // location of your src
         ),
